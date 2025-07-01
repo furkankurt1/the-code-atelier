b4aba18cc5cb1cf44ffc79498342ff3c6c411076
Furkan Atlas <atlas.furkan.kurt@gmail.com>
2025-07-01 17:31:02 +0200

    get project query

--- Boligmappa/Business/Handlers/Project/Queries/GetProjectsQuery.cs
+++ Boligmappa/Business/Handlers/Project/Queries/GetProjectsQuery.cs
@@ -2,25 +2,42 @@
 using DataAccess.Abstract;
 using MediatR;
 
-namespace Business.Handlers.Project.Queries;
-
-public class GetProjectsQuery : IRequest<IDataResult<List<Entities.Concrete.Project>>>
+namespace Business.Handlers.Project.Queries
 {
-    public int UserId { get; set; }
-
-    public class GetProjectsQueryHandler : IRequestHandler<GetProjectsQuery, IDataResult<List<Entities.Concrete.Project>>>
+    public class GetProjectsQuery : IRequest<IDataResult<List<Entities.Concrete.Project>>>
     {
-        private readonly IProjectRepository _projectRepository;
+        public int UserId { get; set; }
 
-        public GetProjectsQueryHandler(IProjectRepository projectRepository)
+        public class GetProjectsQueryHandler : IRequestHandler<GetProjectsQuery, IDataResult<List<Entities.Concrete.Project>>>
         {
-            _projectRepository = projectRepository;
-        }
+            private readonly IProjectRepository _projectRepository;
+            private readonly IUserRepository _userRepository;
 
-        public async Task<IDataResult<List<Entities.Concrete.Project>>> Handle(GetProjectsQuery request, CancellationToken cancellationToken)
-        {
-            var projects = await _projectRepository.GetAllAsync(p => p.CreatedBy == request.UserId);
-            return new SuccessDataResult<List<Entities.Concrete.Project>>(projects);
+            public GetProjectsQueryHandler(IProjectRepository projectRepository, IUserRepository userRepository)
+            {
+                _projectRepository = projectRepository;
+                _userRepository = userRepository;
+            }
+
+            public async Task<IDataResult<List<Entities.Concrete.Project>>> Handle(GetProjectsQuery request, CancellationToken cancellationToken)
+            {
+                var user = await _userRepository.GetByIdAsync(request.UserId);
+                if (user == null)
+                    return new ErrorDataResult<List<Entities.Concrete.Project>>("User not found.");
+
+                List<Entities.Concrete.Project> projects;
+
+                if (user.Role == "Admin")
+                {
+                    projects = (await _projectRepository.GetAllAsync()).ToList();
+                }
+                else
+                {
+                    projects = (await _projectRepository.GetAllAsync(p => p.CreatedBy == request.UserId)).ToList();
+                }
+
+                return new SuccessDataResult<List<Entities.Concrete.Project>>(projects);
+            }
         }
     }
 }
