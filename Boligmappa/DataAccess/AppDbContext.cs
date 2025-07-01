5b225aff583f07a908f4323554dcea1153a230a2
Furkan Atlas <atlas.furkan.kurt@gmail.com>
2025-07-01 13:52:46 +0200

    Some structural changes.

---
+++ Boligmappa/DataAccess/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
---
+++ Boligmappa/Entities/Concrete/User.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Abstract;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } 
+        public string PasswordHash { get; set; } 
+        public string Role { get; set; }
+    }
+}
